d6a05795b8c806fede208e69f4a1d0b7533b9d7e
Karolayne Belo <karolbello98@gmail.com>
2021-06-10 13:31:12 -0300

    questao negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using std::cin;
 using std::cout;
-
+using namespace std;
 const int SIZE = 5; // input size.
 
 int main(void)
 {
     // TODO: Adicione aqui seu código.
 
+    int num, cont=0;
+    cout << "Digite 5 numeros" << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        
+        cin >> num;
+        
+        if (num < 0)
+        {
+            cont = cont+1;   
+        }
+    }
+        cout << cont << endl;
+    
     return 0;
 }
